7bb223224020133d11b95a719ccf51d46349eb6e
Siavash Kaseb <siavashkaseb07@gmail.com>
2024-02-06 15:27:14 +0330

    routing fixed

--- src/app/[locale]/accounts/dashboard/layout.jsx
+++ src/app/[locale]/accounts/dashboard/layout.jsx
@@ -1,17 +1,20 @@
 import { useMessages } from "next-intl";
 import DashboardNav from "../../components/Nav/DashboardNav";
+import GetAccess from "@/app/context/GetAccess";
 
 export default function DashboardLayout({ children }) {
   const t = useMessages();
 
   return (
-    <div className="flex flex-col space-y-6 mt-10 ">
-      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
-        <aside className="hidden w-[200px] flex-col md:flex">
-          <DashboardNav t={t.Dashboard} />
-        </aside>
-        <main>{children}</main>
+    <GetAccess>
+      <div className="flex flex-col space-y-6 mt-10 ">
+        <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
+          <aside className="hidden w-[200px] flex-col md:flex">
+            <DashboardNav t={t.Dashboard} />
+          </aside>
+          <main>{children}</main>
+        </div>
       </div>
-    </div>
+    </GetAccess>
   );
 }
