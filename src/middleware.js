97e1809a3544bc77d7fb6a280e3bab61c6d01193
Siavash Kaseb <siavashkaseb07@gmail.com>
2024-02-08 13:36:39 +0330

    fix middleware

--- src/middleware.js
+++ src/middleware.js
@@ -60,45 +60,45 @@ import { localePrefix, locales } from "./navigations";
 //   matcher: ["/((?!api|_next|.*\\..*).*)"],
 // };
 
-// import createMiddleware from "next-intl/middleware";
-// import { withAuth } from "next-auth/middleware";
+import createMiddleware from "next-intl/middleware";
+import { withAuth } from "next-auth/middleware";
 
-// const intlMiddleware = createMiddleware({
-//   locales: locales,
-//   defaultLocale: "fa",
-//   localeDetection: false,
-//   localePrefix,
-// });
+const intlMiddleware = createMiddleware({
+  locales: locales,
+  defaultLocale: "fa",
+  localeDetection: false,
+  localePrefix,
+});
 
-// const authMiddleware = withAuth(
-//   function onSuccess(req) {
-//     return intlMiddleware(req);
-//   },
-//   {
-//     callbacks: {
-//       authorized: ({ token }) => token != null,
-//     },
-//     pages: {
-//       signIn: "/auth/signin",
-//     },
-//   }
-// );
+const authMiddleware = withAuth(
+  function onSuccess(req) {
+    return intlMiddleware(req);
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token != null,
+    },
+    pages: {
+      signIn: "/auth/signin",
+    },
+  }
+);
 
-// export default function middleware(req) {
-//   const excludePattern = "^(/(" + locales.join("|") + "))?/admin/?.*?$";
-//   const publicPathnameRegex = RegExp(excludePattern, "i");
-//   const isPublicPage = !publicPathnameRegex.test(req.nextUrl.pathname);
+export default function middleware(req) {
+  const excludePattern = "^(/(" + locales.join("|") + "))?/admin/?.*?$";
+  const publicPathnameRegex = RegExp(excludePattern, "i");
+  const isPublicPage = !publicPathnameRegex.test(req.nextUrl.pathname);
 
-//   if (isPublicPage) {
-//     return intlMiddleware(req);
-//   } else {
-//     return authMiddleware(req);
-//   }
-// }
+  if (isPublicPage) {
+    return intlMiddleware(req);
+  } else {
+    return authMiddleware(req);
+  }
+}
 
-// export const config = {
-//   matcher: ["/((?!api|_next|.*\\..*).*)"],
-// };
+export const config = {
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
+};
 
 // import createMiddleware from "next-intl/middleware";
 // import { withAuth } from "next-auth/middleware";
@@ -141,57 +141,3 @@ import { localePrefix, locales } from "./navigations";
 // export const config = {
 //   matcher: ["/((?!api|_next|.*\\..*).*)"],
 // };
-
-import { NextRequest } from "next/server";
-import { withAuth } from "next-auth/middleware";
-import createIntlMiddleware from "next-intl/middleware";
-
-const publicPages = [
-  "/",
-  "/signin",
-  // (/secret requires auth)
-];
-
-const intlMiddleware = createIntlMiddleware({
-  locales,
-  localePrefix: "as-needed",
-  defaultLocale: "fa",
-});
-
-const authMiddleware = withAuth(
-  // Note that this callback is only invoked if
-  // the `authorized` callback has returned `true`
-  // and not for pages listed in `pages`.
-  (req) => intlMiddleware(req),
-  {
-    callbacks: {
-      authorized: ({ token }) => token != null,
-    },
-    pages: {
-      signIn: "/signin",
-    },
-  }
-);
-
-export default function middleware(req) {
-  const publicPathnameRegex = RegExp(
-    `^(/(${locales.join("|")}))?(${publicPages
-      .flatMap((p) => (p === "/" ? ["", "/"] : p))
-      .join("|")})/?$`,
-    "i"
-  );
-  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
-
-  if (isPublicPage) {
-    return intlMiddleware(req);
-  } else {
-    return authMiddleware(req);
-  }
-}
-
-export const config = {
-  // Skip all paths that should not be internationalized
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|apple-touch-icon.png|favicon.svg|images/books|icons|manifest).*)",
-  ],
-};
