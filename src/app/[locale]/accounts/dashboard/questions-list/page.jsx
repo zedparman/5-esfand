faef02721582a7e04b2316c5436025fb4077c28e
SiavashKaseb <siavashkaseb07@gmail.com>
2024-02-15 11:03:19 +0330

    route added

--- src/app/[locale]/accounts/dashboard/questions-list/page.jsx
+++ src/app/[locale]/accounts/dashboard/questions-list/page.jsx
@@ -1,3 +1,23 @@
-export default function QuestionsList() {
-  return <h1>Questions List</h1>;
-}
+import React from "react";
+import axios from "axios";
+import QuestionCard from "@/app/[locale]/components/Questions/QuestionCard";
+
+const getAllQuestions = async () => {
+  const response = await axios.post(
+    `${process.env.BASE_API + "/api/auth/get-user-questions"}`
+  );
+  return response.data;
+};
+const QuestionsPageComponent = async ({ t }) => {
+  const res = await getAllQuestions();
+  console.log({ res });
+  return (
+    <div className="my-5 w-full flex flex-col items-center gap-7">
+      {res?.data?.map((item) => (
+        <QuestionCard key={item._id} t={t} path={"60"} {...item} />
+      ))}
+    </div>
+  );
+};
+
+export default QuestionsPageComponent;
---
+++ src/app/api/auth/get-user-questions/route.js
@@ -0,0 +1,36 @@
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import User from "@/models/User";
+import connectDb from "../../../../../utils/connectDB";
+
+export async function POST(req, res) {
+  try {
+    await connectDb();
+
+    const session = await getServerSession({ req });
+    console.log({ session });
+
+    if (session == null) {
+      return NextResponse.json(
+        {
+          status: "failed",
+          message: "invalid token",
+        },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session?.user?.email });
+    console.log(user);
+
+    return NextResponse.json({ status: "success", data: [] }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json(
+      {
+        status: "failed",
+        message: `Error : ${err}`,
+      },
+      { status: 500 }
+    );
+  }
+}
