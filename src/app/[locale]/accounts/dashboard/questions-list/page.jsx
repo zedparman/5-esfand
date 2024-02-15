431f0b528b5a05481b35505acce55c8a2aea3611
SiavashKaseb <siavashkaseb07@gmail.com>
2024-02-15 21:21:58 +0330

    edit get user

--- src/app/[locale]/accounts/dashboard/questions-list/page.jsx
+++ src/app/[locale]/accounts/dashboard/questions-list/page.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import axios from "axios";
 import QuestionCard from "@/app/[locale]/components/Questions/QuestionCard";
+import { getSession } from "next-auth/react";
 
 const getAllQuestions = async () => {
-  const response = await axios.post(
-    `${process.env.BASE_API + "/api/auth/get-user-questions"}`
-  );
+  const response = await axios.post("http://192.168.1.104:8085/api/auth/get-user-questions");
   return response.data;
 };
-const QuestionsPageComponent = async ({ t }) => {
+const QuestionsPageComponent = async (props) => {
   const res = await getAllQuestions();
-  console.log({ res });
+
   return (
     <div className="my-5 w-full flex flex-col items-center gap-7">
       {res?.data?.map((item) => (
-        <QuestionCard key={item._id} t={t} path={"60"} {...item} />
+        <QuestionCard key={item._id} t={props.t} path={"60"} {...item} />
       ))}
     </div>
   );
--- src/app/api/auth/get-user-questions/route.js
+++ src/app/api/auth/get-user-questions/route.js
@@ -23,7 +23,10 @@ export async function POST(req, res) {
     const user = await User.findOne({ email: session?.user?.email });
     console.log(user);
 
-    return NextResponse.json({ status: "success", data: [] }, { status: 200 });
+    return NextResponse.json(
+      { status: "success", data: user.questions },
+      { status: 200 }
+    );
   } catch (err) {
     return NextResponse.json(
       {
